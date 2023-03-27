52e56c1fd7b10d0b8873777c468acb5cd46eb3df
sina-byn <sina.sina.bayan@protonmail.com>
2023-03-27 14:28:12 +0430

    nft marketplace page added

---
+++ src/pages/Marketplace.tsx
@@ -0,0 +1,14 @@
+// * layouts
+import MainLayout from "../layouts/MainLayout";
+
+// * components
+
+const Marketplace = () => {
+    return (
+        <MainLayout title="NFT Marketplace">
+            NFT Marketplace
+        </MainLayout>
+    );
+};
+
+export default Marketplace;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 
 // * pages
 import MainDashboard from '../pages';
+import Marketplace from '../pages/Marketplace';
 
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path='/marketplace' element={<Marketplace />} />
       <Route path='/' element={<MainDashboard />} />
     </Routes>
   );
