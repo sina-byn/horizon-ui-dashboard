1a3ccb7a091cf0dc53793e93c11600dfa3c23087
sina-byn <sina.sina.bayan@protonmail.com>
2023-07-09 09:44:18 +0330

    status label added

--- src/features/StatusToolbar/OnlineIndicator.tsx
+++ src/features/StatusToolbar/OnlineIndicator.tsx
@@ -16,12 +16,15 @@ const OnlineIndicator = () => {
   }, []);
 
   return (
-    <div
-      className={clsx(
-        'online-indicator w-3.5 h-3.5 rounded-full mt-0.5',
-        isOnline ? 'bg-green-600' : 'bg-red-500'
-      )}
-    />
+    <div className='indicator-container flex items-center gap-x-2'>
+      <div
+        className={clsx(
+          'online-indicator w-3.5 h-3.5 rounded-full mt-0.5',
+          isOnline ? 'bg-green-600' : 'bg-red-500'
+        )}
+      />
+      <span>{isOnline ? 'online' : 'offline'}</span>
+    </div>
   );
 };
 
